cfebc9724ecebb86cc28d018c9a8ead9a3be37f9
Dangnh <dangnh@wav.vn>
2018-06-04 17:38:50 +0700

    no message

---
+++ FootballLeagues/FootballLeagues/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  FootballLeagues
+//
+//  Created by Hai Dang Nguyen on 6/4/18.
+//  Copyright © 2018 Thuy Phan. All rights reserved.
+//
+
+import UIKit
+
+class PlayerModel: NSObject {
+
+}
