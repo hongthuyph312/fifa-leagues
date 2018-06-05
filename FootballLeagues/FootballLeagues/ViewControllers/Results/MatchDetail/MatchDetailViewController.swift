2659cb824ed81f52897e0b956d1e1fed81f8d7ce
Dangnh <dangnh@wav.vn>
2018-06-05 17:27:51 +0700

    no message

---
+++ FootballLeagues/FootballLeagues/ViewControllers/Results/MatchDetail/MatchDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MatchDetailViewController.swift
+//  FootballLeagues
+//
+//  Created by Hai Dang Nguyen on 6/5/18.
+//  Copyright © 2018 Thuy Phan. All rights reserved.
+//
+
+import UIKit
+
+class MatchDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
